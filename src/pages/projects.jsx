b8fb2f9b8a932bf10ef0318ec8789fa90c552b2d
Anurag Singh <anuragsinghofficial2611@gmail.com>
2026-07-12 10:09:11 +0530

    added bloggin app project

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -4,25 +4,47 @@ import demovideo from '../assets/weatherappdemo.mp4'
 import portfoliovideo from '../assets/portfolioproject.mp4'
 import crud from '../assets/CRUD.jpg'
 import lifelink from '../assets/lifelink.png'
+import Bloggingapp from '../assets/Bloggingapp.png'
 
 
 const Projects = () => {
   return (
-    <div id="projects" className="">
+    <div id="projects" className="mt-50">
       <div className="text-white md:not-first:m-30 text-center">
         <motion.h2 className="text-4xl font-extrabold"
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
         >My Works</motion.h2>
         <div>
-          
           <motion.div className="flex justify-between mt-20 flex-col md:flex-row"
-          initial = {{opacity: 0 , y: 20}}
-          whileInView = {{opacity: 1, y: 0}}>
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}>
+            <div></div>
+            <div>
+              <p className="flex justify-between md:ml-60 mb-10 font-bold text-2xl">Blogging App</p>
+              <img src={Bloggingapp}
+                className="w-150 h-70 object-cover"
+              />
+            </div>
+            <div className="mt-10 md:flex-col">
+              <h2 className="font-bold text-2xl">Tech Stack</h2>
+              <p>ReactJS , Framer-motion , ExpressJS , MongoDB , JWT Authentication </p>
+              <h2 className="text-2xl font-bold mt-10 ">Description</h2>
+              <p className="md:w-120 mb-10">
+                Created an Full stack web application in which user can create there account and create a blog post and can see other users blog posts. Implemented JWT Authentiation and Bcrypt for passwords security
+              </p>
+              <a href="https://github.com/Project-Lifelink/LifeLink-Frontend" className="md:w-100 font-bold md:px-30 border-1 py-3 rounded-4xl hover:bg-white hover:text-black">View On GitHub</a>
+            </div>
+            <div></div>
+          </motion.div>
+
+          <motion.div className="flex justify-between mt-20 flex-col md:flex-row"
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}>
             <div></div>
             <div>
               <p className="flex justify-between md:ml-60 mb-10 font-bold text-2xl">LifeLink</p>
-              <img src = {lifelink}
+              <img src={lifelink}
                 className="w-150 h-70 object-cover"
               />
             </div>
@@ -38,8 +60,8 @@ const Projects = () => {
             <div></div>
           </motion.div>
           <motion.div className="flex justify-between mt-20 flex-col md:flex-row"
-          initial = {{opacity: 0 , y: 20}}
-          whileInView = {{opacity: 1, y: 0}}>
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}>
             <div></div>
             <div>
               <p className="flex justify-between md:ml-50 mb-10 font-bold text-2xl">Weather App</p>
@@ -61,20 +83,20 @@ const Projects = () => {
             </div>
             <div></div>
           </motion.div>
-          
+
           <motion.div className="flex justify-between mt-20 flex-col md:flex-row"
-          initial = {{opacity: 0 , y: 20}}
-          whileInView = {{opacity: 1, y: 0}}>
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}>
             <div></div>
             <div>
-              <p className="flex justify-between md:ml-30 mb-10 font-bold text-2xl">CRUD Application in Backend</p>
-              <img src = {crud}
+              <p className="flex justify-between md:ml-10 mb-10 font-bold text-2xl">CRUD and Authentication System in Backend</p>
+              <img src={crud}
                 className="w-150 h-70 object-cover"
               />
             </div>
             <div className="mt-10 md:flex-col">
               <h2 className="font-bold text-2xl">Tech Stack</h2>
-              <p>ExressJS , MongoDB</p>
+              <p>ExressJS , MongoDB , JWT Authentication</p>
               <h2 className="text-2xl font-bold mt-10 ">Description</h2>
               <p className="md:w-120 mb-10">This is an CRUD Application made in Backend using ExpressJS and MongoDB Atlas it store data online on google cloud. Basically it can take information from patient or blood donor and store it on the database and can update , change , delete their data based or requests made to server.  </p>
               <a href="https://github.com/anuragsinghofficial2611/LifeLink-Database-Demo" className="md:w-100 font-bold md:px-30 border-1 py-3 rounded-4xl hover:bg-white hover:text-black">View On GitHub</a>
@@ -82,8 +104,8 @@ const Projects = () => {
             <div></div>
           </motion.div>
           <motion.div className="flex justify-between mt-20 flex-col md:flex-row"
-          initial = {{opacity: 0 , y: 20}}
-          whileInView = {{opacity: 1, y: 0}}>
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}>
             <div></div>
             <div>
               <p className="flex justify-between md:ml-45  mb-10 font-bold text-2xl">Personal Portfolio</p>
@@ -100,15 +122,15 @@ const Projects = () => {
               <h2 className="font-bold text-2xl">Tech Stack</h2>
               <p>ReactJS , TailwindCSS , Framer Motion</p>
               <h2 className="text-2xl font-bold mt-10 m">Description </h2>
-              <p className = "md:w-120 mb-10">This is personal portfolio project to showcase skills and show people about myself and my acheivments to show my capabilities made by ReactJS designed by TailwindCSS and animated through framer motion</p>
+              <p className="md:w-120 mb-10">This is personal portfolio project to showcase skills and show people about myself and my acheivments to show my capabilities made by ReactJS designed by TailwindCSS and animated through framer motion</p>
               <a href="https://github.com/anuragsinghofficial2611/Portfolio" className="md:w-100 font-bold border-1 md:px-30 py-3 rounded-4xl hover:bg-white hover:text-black">View On GitHub</a>
             </div>
             <div></div>
           </motion.div>
           <motion.div
-          initial = {{y: 20, opacity: 0}}
-          whileInView = {{y: 0, opacity: 1}}
-           className = "text-center text-2xl font-bold md:m-40">Currently Working on Many more projects.......</motion.div>
+            initial={{ y: 20, opacity: 0 }}
+            whileInView={{ y: 0, opacity: 1 }}
+            className="text-center text-2xl font-bold md:m-40">Currently Working on Many more projects.......</motion.div>
 
         </div>
       </div>
